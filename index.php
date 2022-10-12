2e90ddcdb4e4a69f4da54ee529a209a71713fc33
Fezur <faizurrheman75@gmail.com>
2022-10-12 19:26:18 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Table</title>
+</head>
+<body>
+    <table class="tbl">
+        <tr class="row">
+            <th>Date</th>
+            <th>Check</th>
+            <th>Description</th>
+            <th>Amount</th>
+        </tr>
+
+        <tr class="data">
+            <td>01/02/2021</td>
+            <td>9251</td>
+            <td>Transiction1</td>
+            <td>$1000</td>
+        </tr>
+        <tr>
+            <td>01/03/2021</td>
+            <td></td>
+            <td>Transiction2</td>
+            <td>$1650</td>
+        </tr>
+        <tr>
+            <td>06/03/2021</td>
+            <td>6347</td>
+            <td>Transiction3</td>
+            <td>-$2000</td>
+        </tr>
+        <tr>
+            <td>04/05/2021</td>
+            <td>1809</td>
+            <td>Transiction4</td>
+            <td>$834</td>
+        </tr>
+    </table>
+
+    
+</body>
+</html>
